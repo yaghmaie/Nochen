a629af53df881a29e3b14961b3977b39479b1a62
Pejman Yaghmaie <2223640+yaghmaie@users.noreply.github.com>
2024-08-18 15:44:32 +0330

    feat: add clean up

--- src/lib.ts
+++ src/lib.ts
@@ -19,6 +19,16 @@ export class NotionConnection {
         this._rootPageId = rootPageId;
     }
 
+    async cleanUp() {
+        const rootContent = await this._client.blocks.children.list({
+            block_id: this._rootPageId,
+        });
+        const cleanUpTasks = rootContent.results.map(({ id }) =>
+            this._client.blocks.delete({ block_id: id })
+        );
+        await Promise.all(cleanUpTasks);
+    }
+
     async createDatabase(dbSchemas: Array<Class>) {
         for (const dbSchema of dbSchemas) {
             const propertySchema = getSchema(dbSchema);
